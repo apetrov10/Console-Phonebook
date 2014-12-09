f1e1d927042c8b45ba99c9d79ee68cc97be40456
Angel Petrov <apetrov10@gmail.com>
2014-12-09 11:17:42 +0200

    class for entries

---
+++ Entry.java
@@ -0,0 +1,15 @@
+package bg.sirma.adressbook;
+
+
+public class Entry {
+	int id;
+	String name;
+	String phoneNumber;
+	
+	Entry (int id, String name, String number){
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = number;
+	}
+
+}
